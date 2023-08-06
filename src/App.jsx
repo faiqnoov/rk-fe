aca9c232be1e0cbcf199c74a27e234e0993c1389
faiqnoov <mfaiqn@gmail.com>
2023-08-07 05:12:54 +0700

    product and brand component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 import { useState, useEffect } from "react";
 
-function Products() {
-  const [products, setProducts] = useState([]);
-
-  const fetchUserData = () => {
-    fetch("http://localhost:8000/api/products")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setProducts(data.data.data);
-      });
-  };
-
-  useEffect(() => {
-    fetchUserData();
-  }, []);
-
-  console.log(products);
-
-  let cat1 = products.filter(product => product.category_id == 1);
-
+function Products({ products }) {
   return (
     <>
       {products.length > 0 ? (
-        <div className="grid grid-cols-3 gap-3">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-3">
           {products.map((product) => (
             <div key={product.id} className="rounded shadow overflow-hidden">
               {product.image ? (
@@ -45,12 +25,81 @@ function Products() {
   );
 }
 
+function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProductData = () => {
+    fetch("http://localhost:8000/api/products")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setProducts(data.data.data);
+      });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, []);
+
+  let brand1 = products.filter(product => product.brand_id == 1);
+  let brand2 = products.filter(product => product.brand_id == 2);
+
+  return (
+    <>
+      <h2 className="font-semibold mb-3">Moderno</h2>
+      <Products products={brand1} />
+      <h2 className="font-semibold mb-3 mt-6">Paris</h2>
+      <Products products={brand2} />
+    </>
+  );
+}
+
+function BrandList() {
+  const [brands, setBrands] = useState([]);
+
+  const fetchBrandData = () => {
+    fetch("http://localhost:8000/api/brands")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setBrands(data.data);
+      });
+  };
+
+  useEffect(() => {
+    fetchBrandData();
+  }, []);
+
+  console.log(brands);
+
+  return (
+    <>
+      <h2 className="font-semibold mb-3">Merek</h2>
+      {brands.length > 0 ? (
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-3 mb-5">
+          {brands.map((brand) => (
+            <div key={brand.id} className="rounded shadow overflow-hidden">
+              <div className="flex justify-center items-center p-1 capitalize">
+                {brand.name}
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="text-center">Loading...</p>
+      )}
+    </>
+  );
+}
+
 function App() {
   return (
-    <div className="container mx-auto px-10 py-20">
+    <div className="container mx-auto p-10">
       <h1 className="text-center text-2xl font-bold mb-5">Katalog Karpet</h1>
-
-      <Products />
+      <BrandList />
+      <ProductList />      
     </div>
   );
 }
